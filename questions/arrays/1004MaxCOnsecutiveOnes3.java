02d9916f857e07acda16f8ae72b3ca11fa3c5652
Samyak Jain <115939022+KayMas2808@users.noreply.github.com>
2025-07-02 12:58:30 +0530

    questions

--- questions/arrays/1004MaxCOnsecutiveOnes3.java
+++ questions/arrays/1004MaxCOnsecutiveOnes3.java
@@ -1,7 +1,24 @@
 package questions.arrays;
+
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        
+        int l = 0, r = 0;
+        int maxLen = 0;
+        int zeroes = 0;
+        while (r < nums.length) {
+            if(nums[r] == 0){
+                zeroes++;
+            }
+            while(zeroes > k){
+                if(nums[l] == 0){
+                    zeroes--;
+                    l++;
+                }
+            }
+            maxLen = Math.max(maxLen, l - r + 1);
+            r++;
+        }
+        return maxLen;
     }
 }
 /*

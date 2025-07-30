88a453d067490f58632b14d97135ef62e9513413
KayMas2808 <sammyakjain2808@gmail.com>
2025-07-31 00:44:16 +0530

    dp

---
+++ dp/Fibo.java
@@ -0,0 +1,14 @@
+//package dp;
+
+public class Fibo {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+
+    public static int fibo(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
